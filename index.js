7bc776271215d3fd91723a5ba7251a26af4fa642
Adam Ibrahim <adamyehialy@gmail.com>
2024-10-27 14:16:22 +0400

    delete gift

--- index.js
+++ index.js
@@ -50,13 +50,23 @@ app.put('/api/gifts/:id', async (req,res) => {
         const updatedProduct = await Gift.findById(id);
         res.status(200).json(updatedProduct);
     } catch (error) {
-        res.status(500).json({message: error.message})
+        res.status(404).json({message: error.message})
     }
 }) 
 
-app.delete('/api/gifts/:id' async (req,res) => { 
-    
-})
+//delete
+app.delete('/api/gifts/:id', async (req,res) => { 
+    try {
+        const {id} = req.params;
+        const gift = await Gift.findByIdAndDelete(id);
+        if(!gift){
+            return res.status(404).json({message: 'could not find gift'})
+        }
+        res.status(200).json(gift);
+    } catch (error) {
+        res.status(500).json({message: error.message});
+    }
+});
 
 // Connect to MongoDB
 mongoose.connect(process.env.MONGODB_URI)

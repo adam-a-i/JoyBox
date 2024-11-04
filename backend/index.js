effcb68b7b735bed83eb27a606686713a31e76bc
Adam Ibrahim <adamyehialy@gmail.com>
2024-11-04 21:44:02 +0400

    file path

--- backend/index.js
+++ backend/index.js
@@ -4,9 +4,10 @@ const mongoose = require('mongoose');
 const Gift = require('./models/gifts.model.js'); // Ensure correct path
 const giftRoute = require('./routes/gift.route');
 const path = require('path'); // Import the path module
-require('dotenv').config();
+require('dotenv').config({ path: path.join(__dirname, '.env') });
 const app = express();
 const cors = require('cors'); // whenver  code works in node terminal but
+const { prototype } = require('module');
 // not in the live server browser do npm install cors
 // sometimes the DB is just slow asf and you have to wait for a bit and refresh
 
@@ -24,12 +25,12 @@ app.use(express.static(path.join(__dirname, '..', 'frontend')));
 app.get('/', (req, res) => { 
     res.send('Welcome to the Gifts API!'); // Updated response message
 });
-
 // Connect to MongoDB
 mongoose.connect(process.env.MONGODB_URI)
     .then(() => {
         console.log('CONNECTED to MongoDB');
-        app.listen(5500, () => {
+        const PORT = 5500;
+        app.listen(PORT, () => {
             console.log('Server is running on port 5500');
         });
     })
